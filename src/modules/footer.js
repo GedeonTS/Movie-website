eb70da6d6dedca7191e14df698b5ba83b14604d1
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-17 13:00:45 +0200

    delete useless spaces

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -1,9 +1,9 @@
-import './footer.css'
+import './footer.css';
 
 const body = document.querySelector('body');
 const footer = document.createElement('footer');
-body.append(footer)
+body.append(footer);
 
-footer.innerHTML = `<h4>Created by Microverse under the CC license</h4>`;
+footer.innerHTML = '<h4>Created by Microverse under the CC license</h4>';
 
 export default footer;
