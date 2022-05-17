e6faa23903123e48009b8a51c4eea243c851ddf7
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-17 21:08:06 +0200

    generate items from api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './index.css';
 import nav from './modules/header.js';
 import footer from './modules/footer.js';
+import movies from './modules/middleSection.js';
 
 const body = document.querySelector('body');
 nav();
-
+movies()
 body.append(footer);
