a05aefc291ad903c3d0ce8aaa657746af45dc051
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-18 11:38:11 +0200

    fix linters

--- src/modules/middleSection.js
+++ src/modules/middleSection.js
@@ -1,5 +1,5 @@
 let arr = [];
-let count=0;
+let count = 0;
 const counter = document.getElementById('count');
 const middleSection = document.getElementById('middle');
 const fetchAll = async () => {
@@ -11,14 +11,10 @@ const movies = async () => {
   await fetchAll();
   arr.forEach((movie) => {
     middleSection.innerHTML += `<article id="${arr.indexOf(movie)}"><img src="${movie.image.medium}"><h5>${movie.name}<h5/><button>comments</button><button>reservations</button><p>${movie.summary}</p></article>`;
-    count++;
-    
-  
+    count += 1;
   });
   counter.innerText = `[${count}]`;
   counter.style.color = 'blue';
-
-
 };
 
 export default movies;
