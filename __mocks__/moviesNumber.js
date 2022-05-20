43b7a071e085d966fcaf4496448a81fbae4aa8c8
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-20 23:47:41 +0200

    add test

--- __mocks__/moviesNumber.js
+++ __mocks__/moviesNumber.js
@@ -1,9 +1,9 @@
-const movies = (arr) => { 
-    let length;
-    arr.forEach((element, i) => {
-        length = i + 1;
-    });
-    return length;
-}
+const movies = (arr) => {
+  let length;
+  arr.forEach((element, i) => {
+    length = i + 1;
+  });
+  return length;
+};
 
 export default movies;
