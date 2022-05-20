18464dffc3bfc499279b28b661db504924d17292
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-20 20:27:00 +0200

    import api related functions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import './index.css';
 import nav from './modules/header.js';
 import footer from './modules/footer.js';
 import movies from './modules/middleSection.js';
+import liking from './modules/likeFeature';
+
 
 const body = document.querySelector('body');
 nav();
 movies();
 body.append(footer);
+
+body.addEventListener('click', () =>{
+    liking()
+})
+
