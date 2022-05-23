9f1a167b4296fb75c0c7406f421ea4f4bdf430c0
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-23 12:07:39 +0200

    add trending link to nav

--- src/modules/header.js
+++ src/modules/header.js
@@ -9,8 +9,10 @@ const nav = () => {
     </a>
     <ul class="nav-items">
       <li class="nav-item"><a href="#" class="navlink">Home</a></li>
+      <li class="nav-item trending"><a href="#" class="navlink">Trending</a></li>
       <li class="nav-item"><a href="#" class="navlink">Categories</a></li>
     </ul>
+    <input type="text" class="search-engine" placeholder="search.."><button class="search-btn" type="submit"><img src="https://img.icons8.com/material-sharp/24/000000/search.png"/></button>
   </nav>
   `;
   headerElement.innerHTML = navElement;
